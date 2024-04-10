a84527f5a24c9238e8dace432738d200cf768446
Joy Schnittker <dev@joyersch.de>
2024-04-10 20:47:32 +0200

    Added EffectsRegistry

---
+++ MonoUtils/Sound/EffectsRegistry.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using IUpdateable = MonoUtils.Logic.IUpdateable;
+
+namespace MonoUtils.Sound;
+
+public class EffectsRegistry : IUpdateable
+{
+    private Dictionary<string, SoundEffect> _effects = new();
+    private Dictionary<string, SoundEffectInstance> _instances = new();
+
+    public void Register(SoundEffect effect, string key)
+    {
+        _effects.Add(key, effect);
+    }
+
+    public SoundEffectInstance? GetInstance(string key)
+    {
+        if (!_effects.ContainsKey(key))
+            return null;
+        var instance = _effects[key].CreateInstance();
+        _instances.Add(key, instance);
+        return instance;
+    }
+
+    public void Update(GameTime gameTime)
+    {
+        List<string> toNuke = new();
+        foreach (var instance in _instances)
+        {
+            if (instance.Value.State == SoundState.Stopped)
+                toNuke.Add(instance.Key);
+        }
+
+        foreach (var instance in toNuke)
+        {
+            var sound = _instances[instance];
+            _instances.Remove(instance);
+            sound.Dispose();
+        }
+    }
+}
