ec68f22398b9e8c0c1aa93deb504025ad38909d4
Joy Schnittker <dev@joyersch.de>
2024-04-13 12:30:57 +0200

    Added more helper extensions

--- MonoUtils/Logic/RectangleExtensions.cs
+++ MonoUtils/Logic/RectangleExtensions.cs
@@ -19,4 +19,16 @@ public static class RectangleExtensions
 
     public static Framework.Vector2 TopLeftCorner(this Framework.Rectangle sender)
         => sender.Location.ToVector2();
+
+    public static Framework.Vector2 LeftCenter(this Framework.Rectangle sender)
+        => sender.Location.ToVector2() + new Framework.Vector2(0, sender.Height / 2);
+
+    public static Framework.Vector2 BottomCenter(this Framework.Rectangle sender)
+        => sender.Location.ToVector2() + new Framework.Vector2(sender.Width / 2, sender.Height);
+
+    public static Framework.Vector2 TopCenter(this Framework.Rectangle sender)
+        => sender.Location.ToVector2() + new Framework.Vector2(sender.Width / 2, 0);
+
+    public static Framework.Vector2 RightCenter(this Framework.Rectangle sender)
+        => sender.Location.ToVector2() + new Framework.Vector2(sender.Width, sender.Height / 2);
 }
---
+++ MonoUtils/Logic/SpartialExtensions.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace MonoUtils.Logic;
+
+public static class SpartialExtensions
+{
+    public static Rectangle GetRectangle(this ISpatial spatial)
+        => new Rectangle(spatial.GetPosition().ToPoint(), spatial.GetSize().ToPoint());
+}
