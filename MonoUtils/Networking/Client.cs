eb0e70fc3581fb2d2789660955d4e8ed63f3ba0f
Joy Schnittker <dev@joyersch.de>
2024-04-21 18:56:12 +0200

    Added Networking

---
+++ MonoUtils/Networking/Client.cs
@@ -0,0 +1,97 @@
+﻿using System.Net.Sockets;
+using System.Text;
+using MonoUtils.Logging;
+
+namespace MonoUtils.Networking;
+
+public class Client : IDisposable
+{
+    private TcpClient _tcpClient;
+    private Thread _connection;
+    private Stream _stream;
+
+    public bool IsConnected { get; private set; }
+
+    public event Action<string> RecievedMessage;
+    public event Action Disconnected;
+    public event Action Connected;
+
+
+    public Client(TcpClient client)
+    {
+        _tcpClient = client;
+    }
+
+    public void Connect()
+    {
+        if (IsConnected)
+            return;
+
+        _stream = _tcpClient.GetStream();
+        _connection = new Thread(ReadFromConnection);
+        _connection.Start(_stream);
+        IsConnected = true;
+        Connected?.Invoke();
+    }
+
+    public void Disconnect()
+    {
+        if (!IsConnected)
+            return;
+
+        if (_connection.IsAlive)
+            _connection.Interrupt();
+        _connection = null;
+        _stream.Close();
+        _stream.Dispose();
+        _stream = null;
+        IsConnected = false;
+        Disconnected?.Invoke();
+    }
+
+    public void Send(string message)
+    {
+        if (!IsConnected)
+            return;
+        var stream = _tcpClient.GetStream();
+        byte[] dataToSend = Encoding.UTF8.GetBytes(message);
+        stream.Write(dataToSend, 0, dataToSend.Length);
+    }
+
+    private void ReadFromConnection(object? obj)
+    {
+        if (_stream is null)
+            return;
+
+        NetworkStream stream = (NetworkStream)_stream;
+        byte[] receivedBuffer = new byte[1024];
+        while (true)
+        {
+            try
+            {
+                int numberOfBytesRead = stream.Read(receivedBuffer, 0, receivedBuffer.Length);
+
+                if (numberOfBytesRead == 0)
+                {
+                    Disconnect();
+                    return;
+                }
+
+                string receivedMessage = Encoding.UTF8.GetString(receivedBuffer, 0, numberOfBytesRead);
+                RecievedMessage?.Invoke(receivedMessage);
+            }
+            catch (Exception exception)
+            {
+                Log.WriteError(exception.Message);
+                Disconnect();
+                return;
+            }
+        }
+    }
+
+    public void Dispose()
+    {
+        _stream?.Dispose();
+        _tcpClient.Dispose();
+    }
+}
---
+++ MonoUtils/Networking/ServerBroker.cs
@@ -0,0 +1,42 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace MonoUtils.Networking;
+
+public class ServerBroker
+{
+    private readonly int _port;
+    private Thread _listenerThread;
+
+    public event Action<Client> ClientConnected;
+
+    public ServerBroker(int port)
+    {
+        _port = port;
+    }
+
+    public void Start()
+    {
+        _listenerThread = new Thread(ListenForClients);
+        _listenerThread.Start(_port);
+    }
+
+    public void Stop()
+    {
+        _listenerThread = new Thread(ListenForClients);
+        _listenerThread.Interrupt();
+        _listenerThread = null;
+    }
+
+    private void ListenForClients(object? obj)
+    {
+        var listener = new TcpListener(IPAddress.Any, _port);
+        listener.Start();
+
+        while (true)
+        {
+            var connection = listener.AcceptTcpClient();
+            ClientConnected?.Invoke(new Client(connection));
+        }
+    }
+}
