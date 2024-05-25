03f5558ce1bc501066fd71f7af77670af618c8be
Joy Schnittker <dev@joyersch.de>
2024-05-25 15:01:53 +0200

    Removed unused usings

--- MonoUtils/Logic/InputReaderMouse.cs
+++ MonoUtils/Logic/InputReaderMouse.cs
@@ -1,7 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using Microsoft.Xna.Framework.Input;
-using MonoUtils.Logging;
+﻿using Microsoft.Xna.Framework.Input;
 
 namespace MonoUtils;
 
